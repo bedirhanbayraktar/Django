04d21c53b73a7ce3432a614be4afd2c52bb75d41
bedirhan4129 <bedirhanbayraktar4129@gmail.com>
2023-03-04 23:51:38 +0300

    'new'

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,15 +1,63 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
+data = {
+"blogs": [
+    {
+        "id": 1,
+        "title": "komple web geliştirme",
+        "image": "1.jpg",
+        "is_active": True,
+        "is_home": False,
+        "description": "çok iyi bir kurs"
+    },
+    {
+        "id": 2,
+        "title": "Python kursu",
+        "image": "2.jpg",
+        "is_active": True,
+        "is_home": True,
+        "description": "çok iyi bir kurs"
+    },
+    {
+        "id": 3,
+        "title": "Django kursu",
+        "image": "3.jpg",
+        "is_active": False,
+        "is_home": True,
+        "description": "çok iyi bir kurs"
+    }
+]
+}
+
 # Create your views here.
 def index(request):
-    return render(request, "blog/index.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request, "blog/index.html", context)
 
 def blogs(request):
-    return render(request, "blog/blogs.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request, "blog/blogs.html", context)
 
 def blog_details(request, id):
+    '''
+    blogs = data["blogs"]
+    selectedBlog = None
+
+    for blog in blogs:
+        if blog["id"] == id:
+            selectedBlog = blog
+    '''
+    blogs = data["blogs"]
+
+    selectedBlog = [blog for blog in blogs if blog["id"]==id][0]
+
     return render(request, "blog/blog-details.html", {
-        "id": id
+
+        "blog": selectedBlog
     })
     
